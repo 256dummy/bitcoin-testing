17407302a51db9293911b4222201b79349ed4756
256dummy <github.dummy@256k1.dev>
2025-07-15 21:07:55 +0100

    empty change

--- src/node/context.cpp
+++ src/node/context.cpp
@@ -2,8 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <node/context.h>
-
 #include <addrman.h>
 #include <banman.h>
 #include <interfaces/chain.h>
@@ -13,6 +11,7 @@
 #include <net.h>
 #include <net_processing.h>
 #include <netgroup.h>
+#include <node/context.h>
 #include <node/kernel_notifications.h>
 #include <node/warnings.h>
 #include <policy/fees.h>
